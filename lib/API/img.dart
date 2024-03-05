a946bf67e95cf32dabdc9830369c48ef2ea5a9a3
Guillaume danckaert <guillaume.danckaert@outlook.com>
2024-03-06 00:24:00 +0100

    first try img.dart

---
+++ lib/API/img.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:archive/archive_io.dart';
+import 'package:path/path.dart' as p;
+
+
+void createTarFile(String sourceDir, String targetFile) async {
+  // Créer une liste de tous les fichiers et sous-répertoires dans le dossier source
+  List<FileSystemEntity> fileList = Directory(sourceDir).listSync(recursive: true);
+  
+
+  // Créer un objet Archive
+  Archive archive = Archive();
+
+  // Ajouter tous les fichiers et sous-répertoires à l'archive
+  for (var file in fileList) {
+    String filePath = file.path;
+    if (FileSystemEntity.isFileSync(filePath)) {
+      List<int> bytes = await (file as File).readAsBytes();
+      ArchiveFile af =  ArchiveFile(filePath, bytes.length, bytes);
+      archive.addFile(af);
+    }
+  }
+
+  // Créer un fichier .tar à partir de l'archive
+  File target = File(targetFile);
+  if (!target.existsSync()) {
+    target.createSync(recursive: true);
+  }
+  target.writeAsBytesSync(TarEncoder().encode(archive));
+}
+
+
+void main(List<String> args) {
+  String sourceFolderPath = 'test';
+  String destinationFilePath = 'data.tar';
+  createTarFile(sourceFolderPath, destinationFilePath);
+}
