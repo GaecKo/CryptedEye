0cd419cd180bb1437594d388dc1428f0f1d21f93
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2024-03-09 21:39:36 +0100

    API: rwm comments so you know what to do

---
+++ lib/API/rwm.dart
@@ -0,0 +1,3 @@
+// TODO: basic write function for general purpose
+// Boolean write_content(String content, String path) -> true if done, false if error/...
+// String read_content(String path) -> List<String> 
