6456934995e1a0ac129e0d54edd67a6e2a591b86
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2024-03-09 21:51:08 +0100

    ctrlr: start of controller

---
+++ lib/controller.dart
@@ -0,0 +1,17 @@
+import 'API/crypter.dart';
+import 'API/img.dart';
+import 'API/rwm.dart';
+
+class Controller {
+  Crypter? crypter;
+  IMG? img;
+  RWM? rwm;
+
+  Controller._create();
+
+  // TODO: create function
+  // think on class instanciation: when should we instantiate the class? Before Login? At Login?
+  
+  // static Future<Controller> create() 
+
+}
