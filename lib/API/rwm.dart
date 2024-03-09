7cff265a7d3af0323b9d9b24e7ea5a92fa09f869
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2024-03-09 21:44:01 +0100

    API: rwm class name, so I can use it in Controller.dart

--- lib/API/rwm.dart
+++ lib/API/rwm.dart
@@ -1,3 +1,7 @@
 // TODO: basic write function for general purpose
 // Boolean write_content(String content, String path) -> true if done, false if error/...
 // String read_content(String path) -> List<String> 
+
+class RWM {
+  
+}
