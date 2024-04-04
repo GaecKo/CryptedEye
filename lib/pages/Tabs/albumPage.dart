8c4132358f55c3b12f62863dc950ad7169551c47
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2024-04-04 17:51:18 +0200

    album fix of overflow (quickly done, WIP)

--- lib/pages/Tabs/albumPage.dart
+++ lib/pages/Tabs/albumPage.dart
@@ -7,90 +7,92 @@ class AlbumsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color.fromRGBO(64, 64, 64, 1),
+        color: const Color.fromRGBO(64, 64, 64, 1),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Row pour aligner les boutons côte à côte
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Premier bouton (Ajouter une photo)
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Code pour afficher la boîte de dialogue ici
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30), // Augmenter la largeur du bouton
-                        backgroundColor: Colors.blue, // Couleur de fond bleu
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20), // Coins arrondis
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Row pour aligner les boutons côte à côte
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Premier bouton (Ajouter une photo)
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Code pour afficher la boîte de dialogue ici
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30), // Augmenter la largeur du bouton
+                          backgroundColor: Colors.blue, // Couleur de fond bleu
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20), // Coins arrondis
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Ajouter une photo',
+                              style: TextStyle(
+                                fontSize: 16, // Taille du texte
+                                fontWeight: FontWeight.bold, // Gras
+                                color: Colors.white, // Couleur du texte blanc
+                              ),
+                            ),
+                            Icon(
+                              Icons.add,
+                              color: Colors.white, // Couleur de l'icône blanche
+                            ),
+                          ],
                         ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Ajouter une photo',
-                            style: TextStyle(
-                              fontSize: 16, // Taille du texte
-                              fontWeight: FontWeight.bold, // Gras
-                              color: Colors.white, // Couleur du texte blanc
-                            ),
-                          ),
-                          Icon(
-                            Icons.add,
-                            color: Colors.white, // Couleur de l'icône blanche
-                          ),
-                        ],
+                    ),
+                    const SizedBox(width: 10), // Petit espacement entre les boutons
+                    // Deuxième bouton (Bouton carré avec le logo +)
+                    Container(
+                      height: 50, // Hauteur du bouton carré
+                      width: 50, // Largeur du bouton carré
+                      decoration: BoxDecoration(
+                        color: Colors.blue, // Couleur de fond bleu
+                        borderRadius: BorderRadius.circular(20), // Coins arrondis
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          // Action lorsque le bouton est pressé
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.white, // Couleur de l'icône blanche
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 10), // Petit espacement entre les boutons
-                  // Deuxième bouton (Bouton carré avec le logo +)
-                  Container(
-                    height: 50, // Hauteur du bouton carré
-                    width: 50, // Largeur du bouton carré
-                    decoration: BoxDecoration(
-                      color: Colors.blue, // Couleur de fond bleu
-                      borderRadius: BorderRadius.circular(20), // Coins arrondis
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        // Action lorsque le bouton est pressé
-                      },
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.white, // Couleur de l'icône blanche
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20), // Espacement entre les boutons et les autres éléments
-              // Autres éléments de la page
-              AlbumRow(
-                albums: [
-                  Album(title: 'Family', imageUrl: 'lib/images/family.jpeg'),
-                  Album(title: 'Travel', imageUrl: 'lib/images/travel.jpeg'),
-                ],
-              ),
-              AlbumRow(
-                albums: [
-                  Album(title: 'Nature', imageUrl: 'lib/images/nature.jpeg'),
-                  Album(title: 'Friends', imageUrl: 'lib/images/friends.jpeg'),
-                ],
-              ),
-              AlbumRow(
-                albums: [
-                  Album(title: 'Events', imageUrl: 'lib/images/events.jpeg'),
-                  Album(title: 'Pets', imageUrl: 'lib/images/pets.jpeg'),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(height: 20), // Espacement entre les boutons et les autres éléments
+                // Autres éléments de la page
+                AlbumRow(
+                  albums: [
+                    Album(title: 'Family', imageUrl: 'lib/images/family.jpeg'),
+                    Album(title: 'Travel', imageUrl: 'lib/images/travel.jpeg'),
+                  ],
+                ),
+                AlbumRow(
+                  albums: [
+                    Album(title: 'Nature', imageUrl: 'lib/images/nature.jpeg'),
+                    Album(title: 'Friends', imageUrl: 'lib/images/friends.jpeg'),
+                  ],
+                ),
+                AlbumRow(
+                  albums: [
+                    Album(title: 'Events', imageUrl: 'lib/images/events.jpeg'),
+                    Album(title: 'Pets', imageUrl: 'lib/images/pets.jpeg'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -155,7 +157,7 @@ class AlbumBox extends StatelessWidget {
           child: Center(
             child: Text(
               album.title,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
