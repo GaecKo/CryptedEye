84cad110f7c4c4b5be0ff9b5264b41c1294223fb
Guillaume danckaert <guillaume.danckaert@outlook.com>
2024-04-22 17:43:56 +0200

    fix img.dart

--- lib/API/img.dart
+++ lib/API/img.dart
@@ -3,15 +3,13 @@ import 'package:archive/archive_io.dart';
 import 'package:path/path.dart' as path;
 
 class IMG {
-  String? unTarFile(String sourceFile)  {
+  String? unTarFile(String sourceFile, String targetDirectory, String tar_name) {
     // Vérifier si le fichier source existe
     File source = File(sourceFile);
     if (!source.existsSync()) {
       print("Le fichier source n'existe pas.");
       return null;
     }
-
-    String? untarredFilePath = sourceFile;
 
     // Lire les données de l'archive tar
     List<int> bytes = source.readAsBytesSync();
@@ -22,67 +20,64 @@ class IMG {
       if (file.isFile) {
         List<int> data = file.content as List<int>;
         String filePath = file.name;
-        File(filePath)
+        String outputFile = '$targetDirectory/$tar_name/$filePath';
+        File(outputFile)
           ..createSync(recursive: true)
           ..writeAsBytesSync(data);
-        print('Fichier extrait: $filePath');
+        print('Fichier extrait: $outputFile');
       }
     }
 
+    source.deleteSync();
     print('Extraction terminée.');
-    return untarredFilePath;
+    return targetDirectory;
   }
-
-  void createTarFile(Directory sourceDir, String targetFile) {
+  void createTarFile(Directory sourceDir) {
     // Créer une liste de tous les fichiers et sous-répertoires dans le dossier source
-    List<FileSystemEntity> fileList =
-        sourceDir.listSync(recursive: true);
+    List<FileSystemEntity> fileList = sourceDir.listSync();
 
     // Créer un objet Archive
     Archive archive = Archive();
 
-    // Ajouter tous les fichiers et sous-répertoires à l'archive
-    for (var file in fileList) {
-      String filePath = file.path;
-      if (FileSystemEntity.isFileSync(filePath)) {
-        List<int> bytes = (file as File).readAsBytesSync();
+    // Ajouter tous les fichiers et sous-répertoires du dernier niveau à l'archive
+    for (var entity in fileList) {
+      if (entity is File) {
+        List<int> bytes = (entity as File).readAsBytesSync();
+        String filePath = entity.path.substring(sourceDir.path.length + 1); // Chemin relatif
         ArchiveFile af = ArchiveFile(filePath, bytes.length, bytes);
         archive.addFile(af);
+      } else if (entity is Directory) {
+        String dirName = entity.path.substring(sourceDir.path.length + 1); // Nom du répertoire
+        addDirectoryContentToArchive(entity, archive, dirName);
       }
     }
 
     // Créer un fichier .tar à partir de l'archive
-    File target = File(targetFile);
-    if (!target.existsSync()) {
-      target.createSync(recursive: true);
+    String tarFileName = '${sourceDir.path}.cryptedEye.tar';
+    File tarFile = File(tarFileName);
+    if (!tarFile.existsSync()) {
+      tarFile.createSync(recursive: true);
     }
-    target.writeAsBytesSync(TarEncoder().encode(archive));
-    print('Tar créer: $targetFile');
-  }
-}
-
-void copyDirectoryContents(String sourceDirPath, String targetDirPath) {
-  Directory sourceDir = Directory(sourceDirPath);
-  Directory targetDir = Directory(targetDirPath);
-
-  if (!sourceDir.existsSync()) {
-    print('Le répertoire source n\'existe pas.');
-    return;
+    tarFile.writeAsBytesSync(TarEncoder().encode(archive));
+    print('Tar créé: $tarFileName');
   }
 
-  if (!targetDir.existsSync()) {
-    targetDir.createSync(recursive: true);
-  }
+  void addDirectoryContentToArchive(Directory directory, Archive archive, String relativePath) {
+    // Récupérer la liste de tous les fichiers et sous-répertoires dans le répertoire
+    List<FileSystemEntity> content = directory.listSync();
 
-  List<FileSystemEntity> contents = sourceDir.listSync(recursive: true);
-  for (FileSystemEntity entity in contents) {
-    String relativePath = path.relative(entity.path, from: sourceDirPath);
-    String destinationPath = path.join(targetDirPath, relativePath);
-
-    if (entity is Directory) {
-      Directory(destinationPath).createSync(recursive: true);
-    } else if (entity is File) {
-      File(entity.path).copySync(destinationPath);
+    // Ajouter tous les fichiers et sous-répertoires à l'archive
+    for (var entity in content) {
+      if (entity is File) {
+        List<int> bytes = (entity as File).readAsBytesSync();
+        String filePath = entity.path.substring(directory.path.length + 1); // Chemin relatif
+        String archivePath = '$relativePath/$filePath';
+        ArchiveFile af = ArchiveFile(archivePath, bytes.length, bytes);
+        archive.addFile(af);
+      } else if (entity is Directory) {
+        String dirName = entity.path.substring(directory.path.length + 1); // Nom du sous-répertoire
+        addDirectoryContentToArchive(entity, archive, '$relativePath/$dirName');
+      }
     }
   }
 }
@@ -90,15 +85,10 @@ void copyDirectoryContents(String sourceDirPath, String targetDirPath) {
 void main(List<String> args) {
   IMG img = IMG();
 
-  String data_path = "test";
+  String data_path = "test_untar/test";
   Directory d = Directory(data_path);
-  String destinationFilePath = 'data.CryptedEye.tar';
+  String tar_name = 'test';
 
-  // img.createTarFile(d, destinationFilePath);
-  //print(img.unTarFile(destinationFilePath));
-
-  String sourceDirPath = 'test/test_tar';
-  String targetDirPath = 'test2';
-
-  copyDirectoryContents(sourceDirPath, targetDirPath);
+  img.createTarFile(d);
+  //img.unTarFile('test.cryptedEye.tar', 'test_untar', tar_name);
 }
