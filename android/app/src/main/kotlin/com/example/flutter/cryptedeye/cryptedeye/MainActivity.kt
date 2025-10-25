3f7fa90cdcbd50a3210e615ad4a298a1d10191cc
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2025-10-25 16:00:27 +0200

    Android: setup repaired

--- android/app/src/main/kotlin/com/example/flutter/cryptedeye/cryptedeye/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter/cryptedeye/cryptedeye/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter.cryptedeye.cryptedeye
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
